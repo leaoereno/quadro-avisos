28ad66ab44b932238e5ee0e62ae5f3900649804d
Rafael M. A. Leão Ereno <57151597+leaoereno@users.noreply.github.com>
2026-06-10 08:20:38 -0300

    Refactor Module class for menu item addition

--- Module.php
+++ Module.php
@@ -1,37 +1,27 @@
 <?php
-
 namespace Modules\NoticeBoardModule;
-
 use Zabbix\Core\CModule;
 use APP;
 use CMenuItem;
 use CWebUser;
 use CView;
-
 class Module extends CModule {
-
     public function init(): void {
         CView::registerDirectory(__DIR__ . '/views');
-
         try {
             $menu = APP::Component()->get('menu.main');
         } catch (\Throwable $e) {
             return;
         }
-
         // Monitoring menu: visible to all users
         $menu->findOrAdd(_('Monitoring'))
             ->getSubMenu()
             ->add((new CMenuItem(_('Notice Board')))->setAction('notice_board.dashboard'));
-
         if (!in_array(CWebUser::getType(), [USER_TYPE_ZABBIX_ADMIN, USER_TYPE_SUPER_ADMIN])) {
             return;
         }
-
         $adminItem = (new CMenuItem(_('Notice Board')))
-            ->setAction('notice_board.view')
-            ->setIcon('zi-support');
-
+            ->setAction('notice_board.view');
         if (CWebUser::getType() === USER_TYPE_SUPER_ADMIN) {
             foreach ($menu->getMenuItems() as $item) {
                 if ($item->getLabel() === _('Administration') || $item->getLabel() === 'Administration') {
@@ -40,7 +30,6 @@ class Module extends CModule {
                 }
             }
         }
-
         // Admin type 2: append to main menu
         $menu->add($adminItem);
     }
